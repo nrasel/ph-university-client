03f3e5e7ade821a968f4a0b6d29eddeba4e8825d
nrasel <nrasel452@gamil.com>
2025-01-11 12:27:21 +0600

    authentication

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import "./index.css";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
 import router from "./routes/routes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}></RouterProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,52 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+import { setUser } from "../redux/features/auth/authSlice";
+import { useAppDispatch } from "../redux/hooks";
+import { verifyToken } from "../utils/verifyToken";
+
 const Login = () => {
-  return <div>This is login</div>;
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      id: "A-0001",
+      password: "admin123",
+    },
+  });
+  const [login, { error }] = useLoginMutation();
+
+  const dispatch = useAppDispatch();
+  const onSubmit = async (data) => {
+    const userInfo = {
+      id: data.id,
+      password: data.password,
+    };
+    const res = await login(userInfo).unwrap();
+    const user = verifyToken(res.data.accessToken);
+    console.log(user);
+    dispatch(setUser({ user: user, token: res.data.accessToken }));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="id">ID:</label>
+          <input {...register("id")} type="text" id="id" name="id" required />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            {...register("password")}
+            type="password"
+            id="password"
+            name="password"
+            required
+          />
+        </div>
+        <Button htmlType="submit">Login</Button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
